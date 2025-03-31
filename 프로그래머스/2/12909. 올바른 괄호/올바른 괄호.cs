6fac0d593e0a05c75754f3318ed141d1eaf41625
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-03-31 12:24:08 +0900

    [level 2] Title: 올바른 괄호, Time: 4.38 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+public class Solution
+{
+    public bool solution(string s)
+    {
+        bool answer = true;
+
+        ParenthesesStack ps = new ParenthesesStack(s);
+        answer = ps.IsParenthesesCouple();
+        Console.WriteLine(answer);
+        return answer;
+    }
+}
+
+public class ParenthesesStack
+{
+    public string parentheses;
+    public Stack<char> stack;
+
+    public ParenthesesStack(string inputS)
+    {
+        parentheses = inputS;
+        stack = new Stack<char>();
+    }
+
+    public bool IsParenthesesCouple()
+    {
+        // () 수가 홀수개 or )로 시작이면 반드시 올바르지 않음
+        if (parentheses.Length % 2 != 0 || parentheses[0] == ')') 
+            return false;
+
+        foreach(char particle in parentheses)
+        {
+            if(stack.Count > 0 && stack.Peek() == '(' && particle == ')' )
+            {
+                stack.Pop();
+                continue;
+            }
+            stack.Push(particle);
+        }
+
+        if (stack.Count > 0)
+            return false;
+        else
+            return true;
+    }
+}
