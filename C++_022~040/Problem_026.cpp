ecf66af13fac75a36f3d20a705180b3a9b26a6b2
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-04-04 00:01:24 +0900

    문제 26, 27 풀이(27은 못풀었다고 봐야함

--- C++_022~040/Problem_026.cpp
+++ C++_022~040/Problem_026.cpp
@@ -1,27 +1,93 @@
 /// <summary>
 /// https://school.programmers.co.kr/learn/courses/30/lessons/92334
 /// 신고 결과 받기
+/// 
+/// 실패했던 이유
+/// unordered_set<string> reportOverKList;를
+/// vector<string> 으로 진행하여 신고당한 사람 중복을 허용했음
+/// 
+/// 저자
+/// <신고당한사람, 신고한 사람 리스트> 이렇게 문제 풀음
 /// </summary>
 
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 #include <unordered_set>
-
 using namespace std;
 namespace P26
 {
+    unordered_map<string, unordered_set<string>> userInfo; // <유저, 신고한 유저 리스트>
+    unordered_map<string, int> reportCountInfo;     // <유저, 신고당한 횟수
+    unordered_set<string> reportOverKList;                 // k 이상 신고당한 유저
+
+    void UpdateReportInfo(const string &nowReport, int reportMaxNum)
+    {
+
+        vector<string> report;
+        stringstream ss(nowReport);
+
+        // 1. report 분할
+        string userId, reportedId;
+        ss >> userId >> reportedId;
+
+        // 2. 신고한 사람에 reportedId 추가 , reportedId에 신고 카운트 ++
+        if (userInfo[userId].find(reportedId) == userInfo[userId].end())
+        {
+            userInfo[userId].insert(reportedId);
+            reportCountInfo[reportedId]++;
+
+            // 3. 신고 카운트가 K 이상이면 신고 누적 리스트에 추가
+            if (reportCountInfo[reportedId] >= reportMaxNum)
+                reportOverKList.insert(reportedId);
+        }
+    }
+
+    int GetReportedIdCount(string searchId)
+    {
+        int count = 0;
+
+        for (string reportedId : reportOverKList)
+        {
+            if (userInfo[searchId].find(reportedId) != userInfo[searchId].end())
+                count++;
+        }
+
+        return count;
+    }
+
     vector<int> solution(vector<string> id_list, vector<string> report, int k) {
         vector<int> answer;
+
+        for (string name : id_list)
+        {
+            userInfo[name];
+        }
+
+        // 신고당한 report 데이터를 userInfo, reportCountInfo, reportOverKList에 추가
+        for (string nowReport : report)
+        {
+            UpdateReportInfo(nowReport, k);
+        }
+
+        // k번 이상 신고당한 사람들을 userInfo[유저아이디]에 얼마나 있는지 세고 개수 반환
+        for (string searchId : id_list)
+        {
+            int nowCount = GetReportedIdCount(searchId);
+            answer.push_back(nowCount);
+        }
+
         return answer;
     }
 }
 
-int main()
-{
-    vector<int> result = P26::solution({ "muzi", "frodo", "apeach", "neo" },
-        { "muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi" });
-    for (int i : result)
-        cout << i << " ";
-}
+//int main()
+//{
+//    vector<int> result = P26::solution({ "muzi", "frodo", "apeach", "neo" },
+//        { "muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi" }, 2);
+//    for (int i : result)
+//        cout << i << " ";
+//}
--- C++_022~040/Problem_027.cpp
+++ C++_022~040/Problem_027.cpp
@@ -8,20 +8,89 @@
 #include <vector>
 #include <algorithm>
 #include <unordered_set>
+#include <set>
+#include <map>
+
 
 using namespace std;
 namespace P27
 {
+    map<string, int> availableAllCourseMap;     // 가능한 코스
+    set<string> availableCourseSet;
+
+    void Combination(string order, string dst, int depth)
+    {
+        if (dst.size() != depth)
+        {
+            for (int i = 0; i < order.size(); i++)
+                Combination(order.substr(i + 1), dst + order[i], depth);
+        }
+        else
+            availableAllCourseMap[dst]++;
+    }
+
+    // avail
+    void AddNewCourse(int size)
+    {
+        // availableAllCourseMap에서 크기가 size 인 것만 vector로 옮김
+        vector<pair<string, int>> nowCourses;
+        for (auto it = availableAllCourseMap.begin(); it != availableAllCourseMap.end(); ++it)
+        {
+            if (it->first.size() == size)
+                nowCourses.push_back(*it);
+        }
+
+        if (nowCourses.empty()) return;
+
+        // 코스가 선택된 순서만큼 정렬
+        sort(nowCourses.begin(), nowCourses.end(), 
+            [](const pair<string, int>& a, const pair<string, int>& b) 
+            {
+                return a.second > b.second;
+            });
+
+        // 2명보다 작으면 지움
+        if (nowCourses[0].second < 2) return;  
+
+        int maxNum = nowCourses[0].second;
+        for (auto& nowCourse : nowCourses)
+        {
+            if (nowCourse.second == maxNum)
+                availableCourseSet.insert(nowCourse.first);
+            else
+                break;
+        }
+    }
+
     vector<string> solution(vector<string> orders, vector<int> course) 
     {
         vector<string> answer;
+
+        // 모든 순열 찾기
+        for (string nowOrder : orders)
+        {
+            sort(nowOrder.begin(), nowOrder.end());
+            for (int courseSize : course)
+            {
+                Combination(nowOrder, "", courseSize);
+            }
+        }
+
+        for (int courseSize : course)
+        {
+            AddNewCourse(courseSize);
+        }
+
+        for (string course : availableCourseSet)
+            answer.push_back(course);
+
         return answer;
     }
 }
 
 int main()
 {
-    vector<string> result = P27::solution({ "ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH" }, { 2,3,4 });
+    vector<string> result = P27::solution({ "ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD" }, { 2,3,5 });
     for (string i : result)
         cout << i << " ";
 }
