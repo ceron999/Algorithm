f4d7854fd60a3093a22ff949b14e311372ac7893
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-04-04 01:06:03 +0900

    트리 문제 준비 완료(28~32)

---
+++ C++_028~032_Tree/ProblemTemplete.cpp
@@ -0,0 +1,20 @@
+/// <summary>
+/// 
+/// </summary>
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+namespace P28
+{
+    
+}
+
+//int main()
+//{
+//    bool result = P22::solution({ "119", "97674223", "1195524421" });
+//
+//    
+//        cout << result << " ";
+//}
---
+++ C++_028~032_Tree/Problem_028.cpp
@@ -0,0 +1,20 @@
+/// <summary>
+/// 
+/// </summary>
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+namespace P28
+{
+    
+}
+
+int main()
+{
+    /*bool result = P28::solution({ "119", "97674223", "1195524421" });
+
+    
+        cout << result << " ";*/
+}
