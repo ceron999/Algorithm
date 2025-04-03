73f2dfc0e06f83b781ef143ce784b62815147697
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-04-03 13:56:37 +0900

    [level 2] Title: 전화번호 목록, Time: 80.58 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) 
+{
+    bool answer = true;
+
+    sort(phone_book.begin(), phone_book.end());
+
+    for (int i = 0; i < phone_book.size() - 1; i++)
+    {
+        int numSIze = phone_book[i].size();
+        if (phone_book[i + 1].substr(0, numSIze) == phone_book[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
