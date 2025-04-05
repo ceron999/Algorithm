e434cf4f7df14bf042edaa785c17bbb87fe15d77
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-04-05 19:50:43 +0900

    Chap10 집합 준비

--- C++_028~032_Tree/Problem_032.cpp
+++ C++_028~032_Tree/Problem_032.cpp
@@ -133,16 +133,16 @@ namespace P32
     }
 }
 
-int main()
-{
-    vector<vector<int>> input = { {5,3}, {11,5}, {13,3}, {3,5}, {6,1},
-                                  {1,3}, {8,6},  {7,2},  {2,2} };
-    vector<vector<int>> result = P32::solution(input);
-
-    for(vector<int> innerVec: result)
-    {
-        for(int i : innerVec)
-            cout << i << " ";
-        cout << endl;
-    }
-}
+//int main()
+//{
+//    vector<vector<int>> input = { {5,3}, {11,5}, {13,3}, {3,5}, {6,1},
+//                                  {1,3}, {8,6},  {7,2},  {2,2} };
+//    vector<vector<int>> result = P32::solution(input);
+//
+//    for(vector<int> innerVec: result)
+//    {
+//        for(int i : innerVec)
+//            cout << i << " ";
+//        cout << endl;
+//    }
+//}
---
+++ C++_033~035_Set/Problem_034.cpp
@@ -0,0 +1,21 @@
+/// <summary>
+/// 
+/// </summary>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+namespace P34
+{
+
+}
+
+//int main()
+//{
+//    vector<int> result = P31::solution( enroll, referral, seller, amount );
+//
+//    for(int i : result)
+//        cout << i << " ";
+//}
