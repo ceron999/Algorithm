da744dd3f15546f34b3b776cd65b6c133116cdb3
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-04-08 21:42:33 +0900

    백트래킹 준비 완료

---
+++ C++_047~053_BackTracking/Problem_052.cpp
@@ -0,0 +1,21 @@
+/// <summary>
+/// 
+/// </summary>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+namespace P52
+{
+
+}
+
+//int main()
+//{
+//    vector<bool> result2 = P52::solution(4, { {'u', '0', '1'},{'u', '2', '3'} ,{'f', '0', '1'} ,{'f', '0', '2'} });
+//
+//    for(int i : result2)
+//        cout << i << " ";
+//}
