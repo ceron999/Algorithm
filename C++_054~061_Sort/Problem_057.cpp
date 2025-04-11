cf08b6f037181d172c0320652158274b1e34aad5
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-04-11 15:20:08 +0900

    정렬 문제 준비 끝(문제 54~61)

---
+++ C++_054~061_Sort/Problem_057.cpp
@@ -0,0 +1,21 @@
+/// <summary>
+/// 
+/// </summary>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+namespace P57
+{
+
+}
+
+//int main()
+//{
+//    vector<bool> result2 = P57::solution(4, { {'u', '0', '1'},{'u', '2', '3'} ,{'f', '0', '1'} ,{'f', '0', '2'} });
+//
+//    for(int i : result2)
+//        cout << i << " ";
+//}
