e22184bb21467553eb7af4621e36fb86b2c93df2
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-04-13 19:53:36 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.cpp
@@ -0,0 +1,58 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int xMax;
+int yMax;
+
+const vector<int> up = { 0,1 };
+const vector<int> down = { 0,-1 };
+const vector<int> left = { -1,0 };
+const vector<int> right = { 1,0 };
+
+vector<int> pos = { 0,0 };
+
+vector<int> operator+=(vector<int>& nowPos, const vector<int>& dir)
+{
+    nowPos = { pos[0] + dir[0], pos[1] + dir[1] };
+    return nowPos;
+}
+
+void Move(const vector<int>& board, string nowKey)
+{
+    switch (nowKey[0])
+    {
+    case 'u':
+        if (pos[1] < yMax)
+            pos += up;
+        break;
+    case 'd':
+        if (pos[1] > (-1) * yMax)
+            pos += down;
+        break;
+    case 'r':
+        if (pos[0] < xMax)
+            pos += right;
+        break;
+    case 'l':
+        if (pos[0] > (-1) * xMax)
+            pos += left;
+        break;
+    }
+}
+
+vector<int> solution(vector<string> keyinput, vector<int> board)
+{
+    vector<int> answer;
+    xMax = board[0] / 2;
+    yMax = board[1] / 2;
+
+    for (string nowKey : keyinput)
+    {
+        Move(board, nowKey);
+    }
+
+    answer = pos;
+    return answer;
+}
