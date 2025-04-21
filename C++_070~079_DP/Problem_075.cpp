d8fee436fb993da5f30818824515674fc708a878
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-04-21 20:15:31 +0900

    문제있는 부분 재업

--- C++_070~079_DP/Problem_075.cpp
+++ C++_070~079_DP/Problem_075.cpp
@@ -1,5 +1,6 @@
 /// <summary>
-/// 
+/// https://school.programmers.co.kr/learn/courses/30/lessons/43105
+/// Á¤¼ö »ï°¢¹ý
 /// </summary>
 
 #include <iostream>
@@ -9,13 +10,38 @@
 using namespace std;
 namespace P75
 {
+    
+    int solution(vector<vector<int>> triangle) {
+        int answer = 0;
 
+        vector<vector<int>> sumTri = triangle;
+
+        for (int i = 1; i < triangle.size(); i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                if (j == 0)
+                    sumTri[i][j] += sumTri[i - 1][j];
+                else if(j == i)
+                    sumTri[i][j] += sumTri[i - 1][j - 1];
+                else
+                {
+                    int left = sumTri[i][j] + sumTri[i - 1][j];
+                    int right = sumTri[i][j] + sumTri[i - 1][j - 1];
+                    sumTri[i][j] = max(left, right);
+                }
+            }
+        }
+
+        answer = *max_element(sumTri[sumTri.size() - 1].begin(), sumTri[sumTri.size() - 1].end());
+        return answer;
+    }
 }
 
 //int main()
 //{
-//    vector<int> result = P75::solution();
+//    vector<vector<int>> triangle = { {7} ,{3, 8},{8, 1, 0},{2, 7, 4, 4},{4, 5, 2, 6, 5} };
+//    int result = P75::solution(triangle);
 //
-//    for (int i : result)
-//        cout << i << " ";
+//        cout << result << " ";
 //}
