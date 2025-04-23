5a346e3c21a9c2e58be19b6d7b08931c06e711e6
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-04-23 16:08:15 +0900

    문제 43 풀이 실패(BFS,DFS 조금 더 공부할 필요성 존재)

--- C++_036~046_Graph/Problem_043.cpp
+++ C++_036~046_Graph/Problem_043.cpp
@@ -6,13 +6,64 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <queue>
 
 using namespace std;
-namespace P43
+namespace P43_Answer
 {
-    int solution(vector<int> info, vector<vector<int>> edges)
-    {
-        int answer = 0;
+
+    vector<vector<int>> tree;
+    vector<int> visited, infoData;
+    int n, answer = 0;
+
+    // 깊이 우선 탐색 (DFS)를 수행하는 함수
+    void dfs(vector<int> currRoot) {
+        int sheep = 0, wolf = 0;
+        // 현재 방문한 경로를 기준으로 양과늑대의 개수를 셈
+        for (int c : currRoot) {
+            if (infoData[c] == 1) wolf++;
+            else sheep++;
+        }
+
+        // 늑대의 수가 양보다 많거나 같으면 종료  
+        if (sheep <= wolf) return;
+
+        // 최대 양의 수 갱신
+        answer = max(answer, sheep);
+
+        for (int i = 0; i < currRoot.size(); i++) {
+            int node = currRoot[i];
+
+            // 현재 노드와 인접한 노드를 순회
+            for (int child : tree[node]) {
+
+                // 이미 방문한 노드는 재방문하지 않음
+                if (visited[child]) continue;
+                // 현재노드를 방문한 경우, 하지 않은 경우 모두 확인
+                visited[child] = true;
+                currRoot.push_back(child);
+                dfs(currRoot);
+                currRoot.pop_back();
+                visited[child] = false;
+            }
+        }
+    }
+
+    int solution(vector<int> info, vector<vector<int>> edges) {
+        n = info.size();
+        tree.resize(n);
+        visited.resize(n, false);
+        infoData = info;
+
+        // 입력값으로 부터 트리 생성
+        for (auto e : edges) {
+            tree[e[0]].push_back(e[1]);
+        }
+
+        visited[0] = true;
+        // 방문여부를 체크하고, 시작 노드부터 탐색을 시작합니다.
+        dfs({ 0 });
         return answer;
     }
 }
@@ -24,7 +75,7 @@ int main()
     {0,1}, {1,2}, {1,4}, {0,8}, {8,7},
     {9,10}, {9,11}, {4,3}, {6,5}, {4,6}, {8,9}
     };
-    int result2 = P43::solution(info, edges);
+    int result2 = P43_Answer::solution(info, edges);
 
         cout << result2 << " ";
 }
