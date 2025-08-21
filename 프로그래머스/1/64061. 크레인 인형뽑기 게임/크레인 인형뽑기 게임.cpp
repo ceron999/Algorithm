5d368ef28ebc087f454370eb723d4adeab1dfdb9
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-08-21 13:04:18 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -1,48 +1,62 @@
-#include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <stack>
 
 using namespace std;
 
+class Container
+{
+    vector<vector<int>> lineContainer;
+
+public:
+    Container(const vector<vector<int>>& board)
+    {
+        lineContainer.resize(board.size(), vector<int>(board[0].size(), 0));
+
+        for (int i = 0;i < board[0].size();i++)
+        {
+            for (int j = 0;j < board.size();j++)
+            {
+                lineContainer[i][j] = board[j][i];
+            }
+        }
+    }
+
+    int GetNumber(const int& line)
+    {
+        for (int i =0; i < lineContainer[line].size(); i++)
+        {
+            if (lineContainer[line][i] == 0)
+                continue;
+
+            int getNum = lineContainer[line][i];
+            lineContainer[line][i] = 0;
+            return getNum;
+        }
+
+        return -1;
+    }
+};
 int solution(vector<vector<int>> board, vector<int> moves) {
-	int answer = 0;
+    int answer = 0;
 
-	vector<stack<int>> stackBoard(board.size());
-	stack<int> pointStack;
-	int nowStackPoint = 0;
+    Container container(board);
 
-	// 1. 스택에 마지막 배열부터 삽입
-	for (int i = board.size() - 1; i >= 0; i--)
-	{
-		nowStackPoint = 0;
-		for (int j = 0; j < board[0].size(); j++)
-		{
-			if(board[i][j] != 0)
-				stackBoard[nowStackPoint].push(board[i][j]);
-			nowStackPoint++;
-		}
-	}
+    stack<int> pointCounter;
+    for (int nowLine : moves)
+    {
+        int nowNum = container.GetNumber(nowLine - 1);
+        if (nowNum == -1)
+            continue;
 
-	// 이제 뽑기 시작
-	for (int stackIndex : moves)
-	{
-		if (!stackBoard[stackIndex - 1].empty())
-		{
-			// 포인트 획득
-			if (!pointStack.empty() && pointStack.top() == stackBoard[stackIndex - 1].top())
-			{
-				answer += 2;
-				pointStack.pop();
-				stackBoard[stackIndex - 1].pop();
-				continue;
-			}
-			pointStack.push(stackBoard[stackIndex - 1].top());
-			stackBoard[stackIndex - 1].pop();
-		}
-	}
+        if (!pointCounter.empty() && pointCounter.top() == nowNum)
+        {
+            pointCounter.pop();
 
-	cout << endl;
-	return answer;
+            answer += 2;
+        }
+        else
+            pointCounter.push(nowNum);
+    }
+    return answer;
 }
