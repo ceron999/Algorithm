4c4d19c57ac9bd5841ad1b68c5c5623696f44fa8
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-09-10 14:38:08 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -1,71 +1,44 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<map>
 
 using namespace std;
 
-#define YES "DA"
-#define NO "NE"
-
-// N : 총 파일 개수
-// pattern : 패턴
-
+// 
 int n;
 string pattern;
-vector<string> patternSplit;
+string front, back;
 vector<string> files;
-
-vector<string> split(const string& input, string delimiter)
-{
-	vector<string> result;
-
-	auto start = 0;
-	auto end = input.find(delimiter);
-
-	while (end != string::npos)
-	{
-		result.push_back(input.substr(start, end - start));
-		start = end + delimiter.size();
-		end = input.find(delimiter, start);
-	}
-
-	result.push_back(input.substr(start));
-	return result;
-}
-
-bool IsContainPattern(string& input)
-{
-	string front = patternSplit[0];
-	string back = patternSplit[1];
-
-	size_t frontSize = front.size();
-	size_t backSize = back.size();
-
-	if (input.size() < frontSize + backSize)
-		return false;
-
-	if (input.compare(0, frontSize, front) != 0) 
-		return false;
-	if (input.compare(input.size() - backSize, backSize, back) != 0) 
-		return false;
-
-	return true;
-}
 
 int main()
 {
-	cin >> n;
-	cin >> pattern;
-	patternSplit = split(pattern, "*");
+	cin >> n >> pattern;
 
+	int pos = pattern.find('*');
+	front = pattern.substr(0, pos);
+	back = pattern.substr(pos + 1);
+	
 	files.resize(n, "");
-
 	for (int i = 0;i < n;i++)
+	{
 		cin >> files[i];
+	}
 
 	for (string file : files)
 	{
-		cout << (IsContainPattern(file) ? YES : NO) << "\n";
+		if (front.size() + back.size() > file.size())
+		{
+			cout << "NE\n";
+			continue;
+		}
+		if (front == file.substr(0, front.size()) && back == file.substr(file.size() - back.size()))
+		{
+			cout << "DA\n";
+		}
+		else
+			cout << "NE\n";
 	}
+
 	return 0;
 }
