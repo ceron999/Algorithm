90f830017cd847f38d111235defe5a85660afa32
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-09-12 13:00:28 +0900

    [Silver III] Title: 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.cc
+++ 백준/Silver/4375. 1/1.cc
@@ -8,21 +8,17 @@ int n;
 
 int main()
 {
-	vector<int> result;
 	while (cin >> n)
 	{
-		int cnt = 1;
 		ll num = 1;
-
+		int cnt = 1;
 		while (num % n != 0)
 		{
 			num = (num * 10 + 1) % n;
 			cnt++;
 		}
-		result.push_back(cnt);
-	}
 
-	for (int i : result)
-		cout << i << "\n";
+		cout << cnt << "\n";
+	}
 
 }
