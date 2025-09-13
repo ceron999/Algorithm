91894530ff62f9d6dcc678c43de8e56119f39682
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-09-13 13:50:12 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -4,34 +4,25 @@
 
 using namespace std;
 
+int ret = 0;
+int n, m, k, map[51][51];
+bool visited[51][51];
+
 int dy[] = { -1,0,1,0 };
 int dx[] = { 0,1,0,-1 };
-
-int map[51][51];
-bool visited[51][51];
-int m, n, k, cnt;
-
-//1 0 0 0 0 0 0 0 0 0
-//1 1 0 0 0 0 0 0 0 0
-//0 0 0 0 1 0 0 0 0 0
-//0 0 0 0 1 0 0 0 0 0
-//0 0 1 1 0 1 0 0 0 0
-//0 0 0 0 0 0 0 0 0 0
-//0 0 0 0 0 0 0 0 0 0
-//0 0 0 0 1 1 1 0 0 0
 
 void dfs(int y, int x)
 {
 	visited[y][x] = true;
 
-	for (int i = 0;i < 4;i++)
+	for (int i = 0; i < 4; i++)
 	{
 		int ny = y + dy[i];
 		int nx = x + dx[i];
 
 		if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-		if (map[ny][nx] == 0) continue;
 		if (visited[ny][nx]) continue;
+		if (map[ny][nx] == 0) continue;
 
 		dfs(ny, nx);
 	}
@@ -42,42 +33,34 @@ int main()
 	int t;
 	cin >> t;
 
-	while (t != 0)
+	while (t--)
 	{
-		t--;
-		cnt = 0;
-
 		cin >> m >> n >> k;
 
-		for (int i = 0;i < k;i++)
+		ret = 0;
+		fill(&map[0][0], &map[0][0] + 51 * 51, 0);
+		fill(&visited[0][0], &visited[0][0] + 51 * 51, 0);
+
+		for (int i = 0; i < k; i++)
 		{
-			int y, x;
+			int x, y;
 			cin >> x >> y;
+
 			map[y][x] = 1;
 		}
 
 		for (int i = 0;i < n;i++)
-			for (int j = 0;j < m;j++)
+		{
+			for (int j = 0;j < m; j++)
 			{
-				if (!visited[i][j] && map[i][j] == 1)
+				if (map[i][j] == 1 && !visited[i][j])
 				{
-					cnt++;
+					ret++;
 					dfs(i, j);
 				}
 			}
-
-		cout << cnt << endl;
-
-		// 초기화
-		for (int i = 0;i < 51;i++)
-		{
-			for (int j = 0;j < 51;j++)
-			{
-				map[i][j] = 0;
-				visited[i][j] = 0;
-			}
 		}
 
-
+		cout << ret<< "\n";
 	}
 }
