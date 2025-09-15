9028be5ec597beec2cb7ffd749eba41299551416
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-09-15 14:17:10 +0900

    [Gold IV] Title: 연구소, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -3,85 +3,78 @@
 
 using namespace std;
 
-int n, m, map[9][9], maxSize = 0;
-int test[9][9];
-bool visited[9][9];
 vector<pair<int, int>> virus;
+vector<pair<int, int>> blank;
+
+int n, m, map[8][8], clone[8][8], ret = 0;
+bool visited[8][8];
 
 int dy[] = { -1,0,1,0 };
 int dx[] = { 0,1,0,-1 };
-// 1. 1을 3개를 둔다.
-// 2. dfs를 통해 2를 퍼트린다.
-// 3. 0 개수를 확인한다.
 
-void spread(int y, int x)
+void dfs(int y, int x)
 {
 	visited[y][x] = true;
 
-	for (int i = 0;i < 4; i++)
+	for (int i = 0;i < 4;i++)
 	{
 		int ny = y + dy[i];
 		int nx = x + dx[i];
 
 		if (ny < 0 || ny >= n || nx < 0 || nx >= m)continue;
-		if (visited[ny][nx])continue;
-		if (test[ny][nx] == 1) continue;
-		
-		test[ny][nx] = 2;
-		spread(ny, nx);
+		if (visited[ny][nx]) continue;
+		if (map[ny][nx] == 1) continue;
+
+		dfs(ny, nx);
 	}
 }
 
-void installWall(int y, int x, vector<pair<int, int>> v)
+int spread()
 {
-	if (v.size() == 3)
+	fill(&visited[0][0], &visited[0][0] + 8 * 8, false);
+	for (auto it : virus)
 	{
-		fill(&visited[0][0], &visited[0][0] + 9 * 9, false);
-		copy(&map[0][0], &map[0][0] + 9 * 9, &test[0][0]);
-
-		for (auto it : virus)
-		{
-			spread(it.first, it.second);
-		}
-
-		int count = 0;
-		for (int i = 0; i < n;i++)
-			for (int j = 0;j < m;j++)
-			{
-				if (test[i][j] == 0)
-					count++;
-			}
-
-		maxSize = max(maxSize, count);
-		return;
+		dfs(it.first, it.second);
 	}
 
-	for(int i= y; i< n;i++)
+	int cnt = 0;
+	for (int i = 0; i < n; i++)
 		for (int j = 0;j < m;j++)
-		{
-			if (map[i][j] == 0)
-			{
-				map[i][j] = 1;
-				v.push_back({ i,j });
-				installWall(i, j,v);
-				map[i][j] = 0;
-				v.pop_back();
-			}
-		}
+			if (map[i][j] == 0 && !visited[i][j])
+				cnt++;
+
+	return cnt;
 }
+
 int main()
 {
 	cin >> n >> m;
-	
-	for (int i = 0;i < n;i++)
-		for (int j = 0;j < m;j++)
+	for(int i =0;i<n;i++)
+		for (int j = 0; j < m;j++)
 		{
 			cin >> map[i][j];
 			if (map[i][j] == 2) virus.push_back({ i,j });
+			if (map[i][j] == 0) blank.push_back({ i,j });
 		}
-	
-	vector<pair<int, int>> v;
-	installWall(0,0, v);
 
-	cout << maxSize<< endl;
+	for (int i = 0;i < blank.size();i++)
+	{
+		for (int j = i + 1; j < blank.size(); j++)
+		{
+			for (int k = j + 1; k < blank.size(); k++)
+			{
+				map[blank[i].first][blank[i].second] = 1;
+				map[blank[j].first][blank[j].second] = 1;
+				map[blank[k].first][blank[k].second] = 1;
+
+				ret = max(ret, spread());
+
+				map[blank[i].first][blank[i].second] = 0;
+				map[blank[j].first][blank[j].second] = 0;
+				map[blank[k].first][blank[k].second] = 0;
+			}
+		}
+	}
+
+	cout << ret;
 }
