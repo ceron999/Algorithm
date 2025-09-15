b8232468b79eb87f9119721645c3e2c9b042f182
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-09-15 13:34:04 +0900

    [Silver III] Title: NBA 농구, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.cc
+++ 백준/Silver/2852. NBA 농구/NBA 농구.cc
@@ -1,108 +1,67 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
 #include<string>
 
 using namespace std;
 
-int n, team1 = 0, team2 = 0;
+int n, aTime = 0, bTime = 0, aScore = 0, bScore = 0, prevTime = 0;
 
-pair<int, int> team1WinTime = { 0,0 };
-pair<int, int> team2WinTime = { 0,0 };
-pair<int, int> lastTime = { 0,0 };
-
-
-pair<int, int> split(const string& input)
+int parseTImeStrToInt(string& time)
 {
-	vector<int> result;
-
-	auto pos = input.find(':');
-
-	string first, second;
-	first = input.substr(0, pos);
-	second = input.substr(pos + 1);
-
-	int min = atoi(first.c_str());
-	int sec = atoi(second.c_str());
-
-	return { min, sec };
-}
-
-void UpdateTime(pair<int, int>& teamTime, pair<int, int> time)
-{
-	teamTime.first += time.first - lastTime.first;
-	teamTime.second += time.second - lastTime.second;
-	if (teamTime.second < 0)
-	{
-		teamTime.first--;
-		teamTime.second += 60;
-	}
-	else if (teamTime.second >= 60)
-	{
-		teamTime.first++;
-		teamTime.second -= 60;
-	}
-}
-
-void Print(pair<int, int>& teamTime)
-{
-	string first, second;
+	int min = stoi(time.substr(0, 2));
+	int sec = stoi(time.substr(3, 2));
 	
-	if (teamTime.first <10)
-	{
-		first = "0";
-		first.push_back(teamTime.first + '0');
-	}
-	else
-	{
-		first = to_string(teamTime.first);
-	}
-	if (teamTime.second < 10)
-	{
-		second = "0";
-		second.push_back(teamTime.second + '0');
-	}
-	else
-	{
-		second = to_string(teamTime.second);
-	}
-	cout << first << ":" << second << "\n";
+	return min * 60 + sec;
+}
 
+string parseTimeToStr(int time)
+{
+	string result;
+
+	int min = time / 60;
+	int sec = time % 60;
+
+	string minStr = "00" + to_string(min);
+	string secStr = "00" + to_string(sec);
+
+	result = minStr.substr(minStr.size() - 2) + ":" + secStr.substr(secStr.size() - 2);
+	return result;
 }
 
 int main()
 {
 	cin >> n;
-	for (int i = 0; i < n;i++)
+
+	while (n--)
 	{
-		int goalTeam;
+		int winTeam;
+		int time;
 		string timeStr;
-		cin >> goalTeam >> timeStr;
+		cin >> winTeam >> timeStr;
 
-		pair<int, int> time = split(timeStr);
-		if (team1 > team2)
+		time = parseTImeStrToInt(timeStr);
+		
+		if (aScore > bScore)
 		{
-			UpdateTime(team1WinTime, time);
-			//cout << "\n임시 : " << team1WinTime.first << " : " << team1WinTime.second << "\n";
+			aTime += (time - prevTime);
 		}
-		else if (team1 < team2)
+		else if (aScore < bScore)
 		{
-			UpdateTime(team2WinTime, time);
+			bTime += (time - prevTime);
 		}
-		lastTime = time;
-
-		if (goalTeam == 1) team1++;
-		else team2++;
+		prevTime = time;
+		winTeam == 1 ? aScore++ : bScore++;
 	}
 
-	if (team1 > team2)
+	if (aScore > bScore)
 	{
-		UpdateTime(team1WinTime, {48,0});
+		aTime += (48 * 60 - prevTime);
 	}
-	else if (team1 < team2)
+	else if (aScore < bScore)
 	{
-		UpdateTime(team2WinTime, { 48,0 });
+		bTime += (48 * 60 - prevTime);
 	}
-	Print(team1WinTime);
-	Print(team2WinTime);
+
+	cout << parseTimeToStr(aTime) << "\n";
+	cout << parseTimeToStr(bTime) << "\n";
 }
