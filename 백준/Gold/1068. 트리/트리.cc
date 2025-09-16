39f98677b3c29279671e723c136f2b40374f6813
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-09-16 13:49:20 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.cc
+++ 백준/Gold/1068. 트리/트리.cc
@@ -1,82 +1,58 @@
 #include<iostream>
 #include<vector>
-#include <queue>
+#include<stack>
 
 using namespace std;
 
-vector<int> adjList[51];
-int n;
-bool visited[51];
+int n, d, ret, root;
+
+vector<int> adjList[54];
+bool visited[54];
 
 // 0 - 1 2
-// 1 - 0 3 4
-// 2 - 0
-// 3 - 1
-// 4 - 1
+// 1 - 3 4
+// 2 - 
+// 3 - 
+// 4 - 
+
+void dfs(int here)
+{
+	visited[here] = true;
+
+	int childCnt = 0;
+	for (int i = 0; i < adjList[here].size(); i++)
+	{
+		int there = adjList[here][i];
+		if (visited[there]) continue;
+		if (there == d) continue;
+
+		childCnt++;
+		dfs(there);
+	}
+
+	if (childCnt == 0) ret++;
+}
 
 int main()
 {
 	cin >> n;
-
-	int root;
 	for (int i = 0; i < n; i++)
 	{
 		int parent;
 		cin >> parent;
-
 		if (parent == -1)
+		{
 			root = i;
-		else
-		{
-			adjList[i].push_back(parent);
-			adjList[parent].push_back(i);
+			continue;
 		}
+		adjList[parent].push_back(i);
 	}
+	cin >> d;
 
-	int eraseNum;
-	cin >> eraseNum;
-	if (eraseNum == root) 
-	{
-		cout << 0;
-		return 0;
-	}
+	if (root != d)
+		dfs(root);
+	else
+		ret = 0;
 
-	for (int i = 0; i < adjList[eraseNum].size(); i++)
-	{
-		int target = adjList[eraseNum][i];
-
-		for (auto it = adjList[target].begin(); it != adjList[target].end();)
-		{
-			if (*it == eraseNum)
-				it = adjList[target].erase(it);
-			else
-				++it;
-		}
-	}
-	adjList[eraseNum].clear();
-
-	int cnt = 0;
-
-	queue<int> q;
-	q.push(root);
-	visited[root] = true;
-	while (q.size())
-	{
-		int node = q.front();
-		q.pop();
-
-		int childCnt = 0;
-		for (int child : adjList[node])
-		{
-			if (!visited[child])
-			{
-				visited[child] = true;
-				q.push(child);
-				childCnt++;
-			}
-		}
-		if (childCnt == 0) cnt++;
-	}
-
-	cout << cnt;
+	cout << ret;
 }
