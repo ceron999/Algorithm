f3854ebc9426697a0617fe161d780f1af3e379e6
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-11-04 15:28:10 +0900

    [Gold IV] Title: 주난의 난(難), Time: 4 ms, Memory: 2480 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.cc
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.cc
@@ -3,76 +3,64 @@
 #include<algorithm>
 #include<string>
 #include<queue>
-
 using namespace std;
 
-int n, m, x, y, x2, y2, cnt = 1;
-queue<pair<int, int>> q;
+int dy[] = { -1,0,1,0 };
+int dx[] = { 0,1,0,-1 };
+
+int n, m, ret, X1,Y1,X2,Y2;
+
 char a[301][301];
 int visited[301][301];
-bool visited_bool[301][301];
-string input;
-
-int dy[] = { -1, 0, 1 ,0 };
-int dx[] = { 0, 1, 0 ,-1 };
-
-bool search(int y, int x)
-{
-	bool flag = false;
-
-	if (a[y][x] == '1' || a[y][x] == '#')
-	{
-		q.push(make_pair(y, x));
-		//cout << y << " " << x << endl;
-		return true;
-	}
-
-	if (a[y][x] == '0' || a[y][x] == '*')
-	{
-		for (int i = 0;i < 4; i++)
-		{
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-
-			if (ny <= 0 || ny > n || nx <= 0 || nx > m) continue;
-			if (visited_bool[ny][nx]) continue;
-
-			visited_bool[ny][nx] = true;
-			if (search(ny, nx)) flag = true;
-		}
-	}
-
-	return flag;
-}
+string s;
+queue<int> q;
 
 int main()
 {
-	cin >> n >> m >> y >> x >> y2 >> x2;
-	visited[y][x] = 1;
+	cin >> n >> m;
+	cin >> Y1 >> X1 >> Y2 >> X2;
 
-	for (int i = 0;i < n;i++)
+	for (int i = 1; i <= n;i++)
 	{
-		cin >> input;
-		for (int j = 0;j < input.size();j++)
+		cin >> s;
+		for (int j = 1; j <= m; j++)
 		{
-			a[i+1][j+1] = input[j];
+			a[i][j] = s[j - 1];
 		}
 	}
 
-	pair<int, int> start = make_pair(y, x);
+	q.push(1000 * Y1 + X1);
+	visited[Y1][X1] = 1;
+	int cnt = 0;
 
-	while (search(y, x))
+	while (a[Y2][X2] != '0')
 	{
-		fill(&visited_bool[0][0], &visited_bool[0][0] + 301 * 301, false);
-
+		cnt++;
+		queue<int> temp;
 		while (q.size())
 		{
-			auto curr = q.front(); q.pop();
-			visited[curr.first][curr.second] = cnt;
-			a[curr.first][curr.second] = '0';
+			int y = q.front() / 1000;
+			int x = q.front() % 1000;
+			q.pop();
+
+			for (int i = 0; i < 4;i++)
+			{
+				int ny = y + dy[i];
+				int nx = x + dx[i];
+
+				if (ny <1 || ny >n || nx < 1 || nx > m || visited[ny][nx]) continue;
+
+				visited[ny][nx] = cnt;
+				if (a[ny][nx] != '0')
+				{
+					a[ny][nx] = '0';
+					temp.push(1000 * ny + nx);
+				}
+				else q.push(1000 * ny + nx);
+			}
 		}
-		cnt++;
+		q = temp;
 	}
 
-	cout << visited[y2][x2];
+	cout << visited[Y2][X2];
 }
