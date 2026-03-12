096f2273cbab6c7f50ddf955a7ffa733302b0e68
CeronMoa <56114106+ceron999@users.noreply.github.com>
2026-03-12 09:39:35 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+
+int main() 
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    cout << 56 * a + 24 * b + 14 * c + 6 * d;
+}
