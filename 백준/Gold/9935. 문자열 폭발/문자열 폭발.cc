39576b192646fb00c4e3399cb9b294243b70de0f
CeronMoa <56114106+ceron999@users.noreply.github.com>
2026-03-12 09:38:14 +0900

    [Gold IV] Title: 문자열 폭발, Time: 80 ms, Memory: 7796 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+
+
+using namespace std;
+
+string S, T, ret;
+
+int main()
+{
+	cin >> S >> T;
+
+	for (char a : S)
+	{
+		ret += a;
+		if (ret.size() >= T.size() && ret.substr(ret.size() - T.size(), T.size()) == T)
+		{
+			ret.erase(ret.end() - T.size(), ret.end());
+		}
+	}
+
+	if (!ret.size()) cout << "FRULA";
+	else cout << ret << "\n";
+	
+}
