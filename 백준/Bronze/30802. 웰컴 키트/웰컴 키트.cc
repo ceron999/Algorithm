aa731bcc8062b1aa437e2af71871454f07b3d148
CeronMoa <56114106+ceron999@users.noreply.github.com>
2026-03-12 09:38:47 +0900

    [Bronze III] Title: 웰컴 키트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.cc
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.cc
@@ -6,27 +6,22 @@
 
 using namespace std;
 
-int n, s, m, l, xl, xxl, xxxl, t, p;
+int n, t, p;
+int a[6];
 
 int main()
 {
 	cin >> n;
-	cin >> s >> m >> l >> xl >> xxl >> xxxl;
+
+	for (int i = 0; i < 6; i++) cin >> a[i];
+
 	cin >> t >> p;
 
 	int ret1 = 0;
-	if (s % t > 0) ret1 += s / t + 1;
-	else ret1 += s / t;
-	if (m % t > 0) ret1 += m / t + 1;
-	else ret1 += m / t;
-	if (l % t > 0) ret1 += l / t + 1;
-	else ret1 +=l / t;
-	if (xl % t > 0) ret1 += xl / t + 1;
-	else ret1 += xl / t;
-	if (xxl % t > 0) ret1 += xxl / t + 1;
-	else ret1 += xxl / t;
-	if (xxxl % t > 0) ret1 += xxxl / t + 1;
-	else ret1 += xxxl / t;
+	for (int i = 0; i < 6; i++)
+	{
+		ret1 += (a[i] + t - 1) / t;
+	}
 
 	cout << ret1 << "\n";
 	cout << n / p << " " << n % p;
