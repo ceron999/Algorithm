eb341e994a7db430c41324b184c29a353da3e3f8
CeronMoa <56114106+ceron999@users.noreply.github.com>
2026-03-12 09:38:05 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+int n;
+string ret;
+
+int main()
+{
+	cin >> n;
+
+	for (int i = 0; i < n / 4; i++)
+	{
+		ret += "long ";
+	}
+	ret += "int";
+
+	cout << ret;
+}
