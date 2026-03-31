11c5853f735eda2eff23fb0af68d802a015a6789
CeronMoa <56114106+ceron999@users.noreply.github.com>
2026-03-31 14:58:19 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 40 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.cc
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.cc
@@ -2,153 +2,129 @@
 #include<vector>
 #include<algorithm>
 #include<queue>
-#include <deque>
-#include<cstring>
 
 using namespace std;
 
-int r, c, t, ret;
-int a[51][51], temp[51][51];
+int a[51][51], n, m, t, ret, temp[51][51];
+vector<pair<int, int>> v1, v2;
 
-// upper dir
 int dy1[] = { 0,-1,0,1 };
 int dx1[] = { 1,0,-1,0 };
 
-// lower dir
 int dy2[] = { 0,1,0,-1 };
 int dx2[] = { 1,0,-1,0 };
 
-pair<int, int> upper;
-pair<int, int> lower;
-
-vector<pair<int, int>> v;
-deque<int> dq;
-
-void spread()
+void mise_go(int dy[], int dx[])
 {
-	memset(temp, 0, sizeof(temp));
+	fill(&temp[0][0], &temp[0][0] + 51 * 51, 0);
+	queue<pair<int, int>> q;
 
-	for (int i = 0;i < r;i++)
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0;j < c;j++)
+		for (int j = 0;j < m;j++)
 		{
-			if (a[i][j] == 0) continue;
-			
-			int dust = a[i][j] / 5;
-			if (dust == 0) continue;
-			
-			for (int idx = 0; idx < 4; idx++)
-			{
-				int ny = i + dy1[idx];
-				int nx = j + dx1[idx];
-				
-				if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
-				if (a[ny][nx] == -1) continue;
-
-				temp[ny][nx] += dust;
-				
-				a[i][j] -= dust;
-			}
+			if (a[i][j] != -1 && a[i][j])
+				q.push({ i,j });
 		}
 	}
 
-	for (int i = 0;i < r;i++)
+	while (q.size())
 	{
-		for (int j = 0;j < c;j++)
+		int y = q.front().first;
+		int x = q.front().second;
+		q.pop();
+
+		int spread = a[y][x] / 5;
+
+		for (int i = 0; i < 4; i++)
+		{
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if (ny < 0 || ny >= n || nx < 0 || nx >= m || a[ny][nx] == -1) continue;
+			temp[ny][nx] += spread;
+			a[y][x] -= spread;
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0;j < m;j++)
 		{
 			a[i][j] += temp[i][j];
 		}
 	}
 }
 
-void move(pair<int, int> start, int dy[], int dx[])
+vector<pair<int, int>> chung(int sy, int sx, int dy[], int dx[])
 {
-	v.clear();
-	dq.clear();
-
-	int starty = start.first;
-	int startx = start.second;
-	int ny = starty;
-	int nx = startx;
-
-	int idx = 0;
+	vector<pair<int, int>> v;
+	int cnt = 0;
+	int y = sy;
+	int x = sx;
 
 	while (true)
 	{
-		ny += dy[idx % 4];
-		nx += dx[idx % 4];
+		int ny = y + dy[cnt];
+		int nx = x + dx[cnt];
 
-		if (ny < 0 || ny >= r || nx < 0 || nx >= c)
+		if (ny == sy && nx == sx) break;
+		if (ny < 0 || ny >= n || nx < 0 || nx >= m)
 		{
-			ny -= dy[idx % 4];
-			nx -= dx[idx % 4];
-			idx++;
-			continue;
-		}if (a[ny][nx] == -1) break;
-
-		v.push_back({ ny, nx });
-		dq.push_back(a[ny][nx]);
+			cnt++;
+			ny = y + dy[cnt];
+			nx = x + dx[cnt];
+		}
+		if (ny == sy && nx == sx) break;
+		y = ny; x = nx;
+		v.push_back({ ny,nx });
+		
 	}
 
-	dq.push_front(0);
-	dq.pop_back();
+	return v;
+}
 
-
-	for (auto it : v)
+void go(vector<pair<int, int>>& v)
+{
+	for (int i = v.size() - 1; i > 0; i--)
 	{
-		//cout << "{ " << it.first << " " << it.second << " " << dq.front() << " }\n";
-
-		int y = it.first;
-		int x = it.second;
-
-		a[y][x] = dq.front();
-		dq.pop_front();
+		a[v[i].first][v[i].second] = a[v[i - 1].first][v[i - 1].second];
 	}
+		a[v[0].first][v[0].second] = 0;
+		return;
 }
 
 int main()
 {
-	cin >> r >> c >> t;
-
-	bool isSet = false;
-	for (int i = 0;i < r;i++)
+	cin >> n >> m >> t;
+	bool flag = 1;
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0;j < c;j++)
+		for (int j = 0; j < m; j++)
 		{
 			cin >> a[i][j];
-
-			if (!isSet && a[i][j] == -1)
+			if (a[i][j] == -1)
 			{
-				isSet = true;
-				upper = { i,j };
-				lower = { i + 1,j };
+				if (flag) 
+				{
+					v1 = chung(i, j, dy1, dx1);
+					flag = false;
+				}
+				else v2 = chung(i, j, dy2, dx2);
 			}
 		}
 	}
-
-	while (t--)
+	while (t--) 
 	{
-		spread();
-		move(upper, dy1,dx1);
-		move(lower, dy2,dx2);
+		mise_go(dy1, dx1);
+		go(v1);
+		go(v2);
 	}
-	
-	for (int i = 0;i < r;i++)
+
+	for (int i = 0; i < n; i++) 
 	{
-		for (int j = 0;j < c;j++)
+		for (int j = 0; j < m; j++) 
 		{
-			if (a[i][j] != -1) ret += a[i][j];
+			if (a[i][j] != -1)ret += a[i][j];
 		}
 	}
-	cout << ret<< "\n\n";
-
-	/*for (int i = 0;i < r;i++)
-	{
-		for (int j = 0;j < c;j++)
-		{
-			cout << a[i][j] << " ";
-		}
-		cout << endl;
-	}*/
-
+	cout << ret << "\n";
 }
